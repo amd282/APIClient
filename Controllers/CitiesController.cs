3e58b68374be519c603135bc62149295f0e848a7
amd282 <amd282@yahoo.com>
2023-11-28 20:13:29 -0500

    added POST, PUT, DELETE methods and view pages

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -1,6 +1,7 @@
 ﻿using APIClient.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace APIClient.Controllers
 {
@@ -27,6 +28,84 @@ namespace APIClient.Controllers
                 citieslist = JsonConvert.DeserializeObject<List<Cities>>(data);
             }
             return View(citieslist);
+        }
+
+        [HttpGet] 
+        public IActionResult Create() 
+        {
+            return View();        
+        }
+
+        [HttpPost]
+        public IActionResult Create(Cities model) 
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "/Cities/PostCities", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "City Created"; 
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+            return View();        
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            Cities city = new Cities();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Cities/GetCities/" + id).Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                city = JsonConvert.DeserializeObject<Cities>(data);
+            }
+            return View(city);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Cities model) 
+        {
+            string data = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = _client.PutAsync(_client.BaseAddress + "/Cities/PutCity", content).Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) 
+        {
+            Cities city = new Cities();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Cities/GetCities/" + id).Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                string data = response.Content?.ReadAsStringAsync().Result;
+                city = JsonConvert.DeserializeObject<Cities>(data);
+            }
+            return View(city);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id) 
+        {
+            HttpResponseMessage response = _client.DeleteAsync(_client.BaseAddress + "/Cities/DeleteCities/" + id).Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                return RedirectToAction("Index");
+            }
+            return View(response);
         }
     }
 }
--- Controllers/WeatherDataController.cs
+++ Controllers/WeatherDataController.cs
@@ -1,6 +1,7 @@
 ﻿using APIClient.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace APIClient.Controllers
 {
@@ -27,6 +28,84 @@ namespace APIClient.Controllers
                 weatherdatalist = JsonConvert.DeserializeObject<List<WeatherData>>(data);
             }
             return View(weatherdatalist);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(WeatherData model)
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "/WeatherData/PostWeatherData", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "Weather data Created";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            WeatherData weatherdata = new WeatherData();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/WeatherData/GetWeatherData/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                weatherdata = JsonConvert.DeserializeObject<WeatherData>(data);
+            }
+            return View(weatherdata);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(WeatherData model)
+        {
+            string data = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = _client.PutAsync(_client.BaseAddress + "/WeatherData/PutWeatherData", content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            WeatherData weatherdata = new WeatherData();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/WeatherData/GetWeatherData/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content?.ReadAsStringAsync().Result;
+                weatherdata = JsonConvert.DeserializeObject<WeatherData>(data);
+            }
+            return View(weatherdata);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            HttpResponseMessage response = _client.DeleteAsync(_client.BaseAddress + "/WeatherData/DeleteWeatherData/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(response);
         }
     }
 }
