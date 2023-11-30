2e8a743d68991d9342cdc5bac39bec005c293cdb
AlexDamovski <112990760+AlexDamovski@users.noreply.github.com>
2023-11-30 11:50:49 -0500

    Changes to how data is displayed

--- DTO/WeatherDataDto.cs
+++ DTO/WeatherDataDto.cs
@@ -2,12 +2,11 @@
 {
     public class WeatherDataDto
     {
-        public int Id { get; set; }
+        public int id { get; set; } 
         public int CityId { get; set; }
         public string Conditions { get; set; }
         public decimal Temperature { get; set; }
         public decimal WindSpeed { get; set; }
         public DateTime Date { get; set; }
-        public string Name { get; set; }
     }
 }
